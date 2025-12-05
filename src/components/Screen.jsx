4dfc8b47c912bd439927d6003bd9b7780f29f693
jeffkaberagithara-guru <jeffkaberagithara@gmail.com>
2025-12-05 20:12:36 +0300

    Modified screen.jsx file.

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -15,7 +15,7 @@ function Screen() {
     <div className={`min-h-screen flex flex-col items-center justify-center transition-colors duration-500 ${ison ? 'bg-white' : 'bg-black'}`}>
       
           <p className="text-2xl text-blue-400 mb-5">Status: {ison ? 'It is bright' : 'It is dark'}</p>
-          <button onClick={handleToggle} className="bg-blue-400 rounded-4xl text-white py-5 px-10">{ison ? 'Turn off' : 'Turn on' }</button>
+          <button onClick={handleToggle} className="bg-blue-400 cursor-pointer translation transform hover:scale-105 rounded-4xl text-white py-5 px-10">{ison ? 'Turn off' : 'Turn on' }</button>
     </div>
   );
 }
